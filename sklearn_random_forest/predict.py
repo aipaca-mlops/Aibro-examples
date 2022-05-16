8f015afe04fec4e900dd8778272f989a14a63d87
SGGb0nd <lilounan199@gmail.com>
2022-05-16 14:51:37 +0800

    add data folder

--- sklearn_random_forest/predict.py
+++ sklearn_random_forest/predict.py
@@ -5,19 +5,6 @@ import pickle
 import numpy as np
 
 
-#Load dataset
-iris = datasets.load_iris()
-X, y = iris['data'], iris['target']
-X_train, X_test, y_train, _ = train_test_split(X, y, test_size=0.3) # 70% training and 30% test
-
-np.savez("./data/iris.npz", X_test, )
-
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-filename = './model/sklearn-rf.sav'
-pickle.dump(model, open(filename, 'wb'))
-
 def load_model():
     # Portuguese to English translator
     rf_model = pickle.load(open('model/sklearn-rf.sav', 'rb'))
@@ -31,4 +18,5 @@ def run(model):
     return result
 
 # if __name__ == "__main__":
-#     run(load_model())
+#     result = run(load_model())
+#     print(result)
